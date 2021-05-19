9f986368122d96d016d584c9468eb688f5a203e2
RLuzio <46633368+RLuzio@users.noreply.github.com>
2021-05-18 23:33:28 -0700

    Delete degree_table.sql

--- degree_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS college_degrees(
-        professor_ssn NUMERIC(9) NOT NULL,
-        PRIMARY KEY (professor_ssn),
-        FOREIGN KEY (professor_ssn) REFERENCES professor(ssn)
-);
